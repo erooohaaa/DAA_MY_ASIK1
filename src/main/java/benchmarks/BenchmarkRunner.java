292ba53e571a7588c2a75a3accc235f6e048280b
Yerkanat <erkanatm249@gmail.com>
2025-09-28 16:50:20 +0500

    Setup JMH benchmarks

---
+++ src/main/java/benchmarks/BenchmarkRunner.java
@@ -0,0 +1,18 @@
+package benchmarks;
+
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+public class BenchmarkRunner {
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include(SelectionBenchmark.class.getSimpleName())
+                .include(SortingBenchmark.class.getSimpleName())
+                .forks(1)
+                .build();
+
+        new Runner(opt).run();
+    }
+}
---
+++ src/main/java/benchmarks/SortingBenchmark.java
@@ -0,0 +1,49 @@
+package benchmarks;
+
+import algo.sort.MergeSort;
+import algo.sort.QuickSort;
+import org.openjdk.jmh.annotations.*;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@State(Scope.Thread)
+@Warmup(iterations = 2, time = 1)
+@Measurement(iterations = 3, time = 1)
+@Fork(1)
+public class SortingBenchmark {
+
+    @Param({"1000", "10000", "50000"})
+    private int size;
+
+    private int[] data;
+
+    @Setup(Level.Iteration)
+    public void setup() {
+        Random rnd = new Random(42);
+        data = rnd.ints(size, 0, Integer.MAX_VALUE).toArray();
+    }
+
+    @Benchmark
+    public int[] mergeSort() {
+        int[] copy = data.clone();
+        MergeSort.sort(copy);
+        return copy;
+    }
+
+    @Benchmark
+    public int[] quickSort() {
+        int[] copy = data.clone();
+        QuickSort.sort(copy);
+        return copy;
+    }
+
+    @Benchmark
+    public int[] arraysSort() {
+        int[] copy = data.clone();
+        Arrays.sort(copy);
+        return copy;
+    }
+}
